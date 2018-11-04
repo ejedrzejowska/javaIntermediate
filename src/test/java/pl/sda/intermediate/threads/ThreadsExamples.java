7a8c9246dfc579d2f096aa9b3c7f1e96e177e4bb
Ewa Jedrzejowska <efciajed@gmail.com>
2018-11-04 10:46:00 +0100

    Threads

--- src/test/java/pl/sda/intermediate/threads/ThreadsExamples.java
+++ src/test/java/pl/sda/intermediate/threads/ThreadsExamples.java
@@ -1,8 +1,43 @@
 package pl.sda.intermediate.threads;
 
 import org.junit.jupiter.api.Test;
+import pl.sda.intermediate.threads.bank.Bank;
+import pl.sda.intermediate.threads.bank.ClientActionRunnable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ThreadsExamples {
+
+    @Test
+    void bankExampleSynchronized(){
+        for (int i = 0; i < 1000; i++) {
+            new ClientActionRunnable().run();
+        }
+    }
+
+    @Test
+    void bankExampleSeparateThreads(){
+        List<Thread> threadList = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            threadList.add(new Thread(new ClientActionRunnable()));
+        }
+        for (Thread thread : threadList) {
+            thread.start(); //nie run, bo run zapusci w watku main
+        }
+        for (Thread thread : threadList) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("balans po: " + Bank.getBalance());
+        System.out.println("Bilans atomowy na koniec: " + Bank.getAtomicBalance());
+        System.out.println("Liczba operacji: " +  Bank.getCounter());
+    }
+
     @Test
     public void runnableBasics(){
         Runnable runnable = () -> System.out.println(Thread.currentThread().getName() + " Lambda runnable");
